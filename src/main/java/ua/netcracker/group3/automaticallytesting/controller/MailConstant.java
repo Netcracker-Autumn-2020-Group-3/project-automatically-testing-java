600be0960bb35917a1324358149a752e7280294b
Xet111 <28compasov@gmail.com>
2020-11-16 13:35:37 +0200

    Some changes

--- src/main/java/ua/netcracker/group3/automaticallytesting/controller/MailConstant.java
+++ src/main/java/ua/netcracker/group3/automaticallytesting/controller/MailConstant.java
@@ -2,6 +2,6 @@ package ua.netcracker.group3.automaticallytesting.controller;
 
 public interface MailConstant {
     String EMAIL = "llopatokecapl@gmail.com";
-    String EMAIL_PASS = "osalzdiwmaiboasa";
+    String EMAIL_PASS = "wowugvclyvnhvxzr";
     String EMAIL_THEME = "Credentials for testing app";
 }
