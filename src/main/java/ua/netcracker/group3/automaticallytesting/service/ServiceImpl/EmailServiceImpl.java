ccf6eb05c27985312555e70644fa8e44f8d3c3cc
Xet111 <28compasov@gmail.com>
2020-11-21 18:35:29 +0200

    Added sending password on email feature

--- src/main/java/ua/netcracker/group3/automaticallytesting/service/ServiceImpl/EmailServiceImpl.java
+++ src/main/java/ua/netcracker/group3/automaticallytesting/service/ServiceImpl/EmailServiceImpl.java
@@ -1,11 +1,28 @@
 package ua.netcracker.group3.automaticallytesting.service.ServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import ua.netcracker.group3.automaticallytesting.controller.Constant.MailConstant;
+import ua.netcracker.group3.automaticallytesting.model.User;
 
 @Service
 public class EmailServiceImpl {
 
-    public void sendCredentials(){
+    private JavaMailSender mailSender;
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
 
+    public void sendCredentialsByEmail(User user){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(user.getEmail());
+        message.setSubject(MailConstant.EMAIL_THEME);
+        message.setText(user.getPassword());
+
+        mailSender.send(message);
     }
 }
