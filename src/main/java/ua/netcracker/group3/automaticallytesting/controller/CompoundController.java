9b94e63f5f816c65a97f05982a4f115671d51d82
Xet111 <28compasov@gmail.com>
2020-11-22 21:29:28 +0200

    Working Controller for Compound edit

---
+++ src/main/java/ua/netcracker/group3/automaticallytesting/controller/CompoundController.java
@@ -0,0 +1,29 @@
+package ua.netcracker.group3.automaticallytesting.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import ua.netcracker.group3.automaticallytesting.model.Compound;
+import ua.netcracker.group3.automaticallytesting.service.CompoundService;
+
+@RestController
+public class CompoundController {
+    private CompoundService compoundService;
+
+    @Autowired
+    public CompoundController(CompoundService compoundService) {
+        this.compoundService = compoundService;
+    }
+
+    @GetMapping("/compound/{id}")
+    @PreAuthorize("hasRole('ADMIN')" + "|| hasRole('MANAGER')" + "|| hasRole('ENGINEER')")
+    public Compound editCompound(@PathVariable long id) throws Exception{
+        return compoundService.getCompoundById(id);
+    }
+    @PostMapping("/compound/{id}/edit")
+    @PreAuthorize("hasRole('ADMIN')" + "|| hasRole('MANAGER')" + "|| hasRole('ENGINEER')")
+    public String updateCompound(@PathVariable long id, @RequestBody Compound compound){
+        compoundService.updateCompound(compound, id);
+        return "ok";
+    }
+}
